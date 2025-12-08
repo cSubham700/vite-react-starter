b661876c967ae23373fe56444a5030039d398f29
csubham7000 <119515129+cSubham700@users.noreply.github.com>
2025-12-08 20:28:33 +0530

    map func, conditional rendering

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './components/Nav'
 
 const Home = () => {
+
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count+1);
+    console.log(count);
+    
+  }
+
   return (
    <>
    <Nav/>
    <div className='home'>
     
     <h1>Home Page</h1>
+    <h1>{count}</h1>
+    <button onClick={handleClick}>Click</button>
     </div>
    </>
   )
---
+++ src/people.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        id:"1",
+        imgSrc:"https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fwww.gravatar.com%2Favatar%2F2c7d99fe281ecd3bcd65ab915bac6dd5%3Fs%3D250",
+        name: "John Doe",
+        role: "Frontend Web Developer",
+        type: "employee"
+    },
+    {
+        id:"2",
+        imgSrc:"https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/http%3A%2F%2Fplacebeard.it%2F250%2F250",
+        name: "Luke Harper",
+        role: "Backend Web Developer",
+        type: "employee"
+    },
+    {
+        id:"3",
+        imgSrc:"https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fi.pravatar.cc%2F250%3Fu%3Dmail%40ashallendesign.co.uk",
+        name: "Kane Williams",
+        role: "UI/UX designer",
+        type: "freelancer"
+    },
+]
